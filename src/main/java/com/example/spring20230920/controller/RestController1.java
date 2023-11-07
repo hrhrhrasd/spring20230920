90bad382b00ea13262d0ee63c965199d22230852
hrhrhrasd <gmleh1245@gmail.com>
2023-11-07 17:47:08 +0900

    api/main1/sub2,3 컨트롤러 생성

--- src/main/java/com/example/spring20230920/controller/RestController1.java
+++ src/main/java/com/example/spring20230920/controller/RestController1.java
@@ -1,5 +1,8 @@
 package com.example.spring20230920.controller;
 
+import com.example.spring20230920.dao.MyDao10;
+import com.example.spring20230920.domain.MyDto34;
+import com.example.spring20230920.service.MyService2;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,9 +14,23 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/api/main1")
 public class RestController1 {
 
+    private final MyDao10 dao;
+
     @GetMapping("/sub1")
     @ResponseBody
     public String method1() {
         return "hello boot app!!";
+    }
+
+    @GetMapping("/sub2")
+    @ResponseBody
+    public String method2() {
+        return dao.getCustomerNameById(5);
+    }
+
+    @GetMapping("/sub3")
+    @ResponseBody
+    public MyDto34 method3() {
+        return dao.getCustomerById(5);
     }
 }
